2d3db21903761ae7b74b2b275606391f913f0d78
BuildTools <amp7368@rit.edu>
2020-10-16 01:55:06 -0400

    added a table like structure and header to the Calendar

--- src/main/java/apple/excursion/discord/commands/general/CommandCalendar.java
+++ src/main/java/apple/excursion/discord/commands/general/CommandCalendar.java
@@ -1,13 +1,13 @@
 package apple.excursion.discord.commands.general;
 
 import apple.excursion.discord.commands.DoCommand;
-import apple.excursion.discord.reactions.messages.DailyTaskListMessage;
+import apple.excursion.discord.reactions.messages.CalendarMessage;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class CommandCalendar implements DoCommand {
 
     @Override
     public void dealWithCommand(MessageReceivedEvent event) {
-        new DailyTaskListMessage(event.getChannel());
+        new CalendarMessage(event.getChannel());
     }
 }
